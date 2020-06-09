d39e93a096a6f3356803ed262f53cfb3c28550af
Keino Baird <keinobaird@Keinos-Air.fios-router.home>
2020-06-08 23:37:57 -0400

    predictions forms etc

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -6,10 +6,17 @@ home_routes = Blueprint("home_routes", __name__)
 
 @home_routes.route("/")
 def index():
+    return render_template("prediction_form.html")
+
+    return f"Hello World! {x}"
+
+@home_routes.route("/")
+def index():
     print("Visiting the homepage")
     x = 2 + 2
 
     return f"Hello World! {x}"
+
 
 @home_routes.route("/about")
 def about():
